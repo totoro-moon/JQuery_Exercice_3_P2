fadafba6c7d463b5b12d806ef2dcf75723bf17bd
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-05-03 09:33:28 +0200

    JQuery_Exercice_3_P2

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#cacher").click(function () {
+        $("#texte").hide();
+    });
+    $("#afficher").click(function () {
+        $("#texte").show();
+    });
+});
+// methode toogle bascule entre (affiche et cache)
+// $(document).ready(function () {
+//     $("#cacher").click(function () {
+//         $("#texte").tooggle();
+//     });
+// });
+
